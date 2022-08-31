8eeea36cd58b73ff41f8b469c1dc16b5eee50149
aroxan9999999 <112550824+aroxan9999999@users.noreply.github.com>
2022-08-31 17:50:02 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "5123731440:AAFla82Q5kn1LTISEfosy2hrIC4aC-i50iU"
+token = "your token"
 arguments_sample = ["latitude", "longitude", "hotel_count", "photo_count", "checkin_date", "checkout_date",
                     "sort_order", "min", "max", "centr_hotel"]
 order = [0]
